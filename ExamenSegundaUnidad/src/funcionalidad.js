a39e4d6f1dd25d147318df127462115bafe8fc06
Rafael-cmd-html <armandocastillejos086@gmail.com>
2023-09-16 15:37:13 -0700

    Creación de escenario completo

--- ExamenSegundaUnidad/src/funcionalidad.js
+++ ExamenSegundaUnidad/src/funcionalidad.js
@@ -10,19 +10,21 @@ class Limites{
     static width =40;
     static height=40;
 
-    constructor({posicion}){ //Para no pasar todos los parámetros de posicion, creamos un objeto
+    constructor({posicion, imagen}){ //Para no pasar todos los parámetros de posicion, creamos un objeto
 
         this.posicion= posicion;
         this.width=40;
         this.height=40;
+        this.imagen=imagen;
 
     }
 
     dibujarLimite(){ //Creamos una función que nos permite dibujar los límites (Cuadrados)
 
-        contexto.fillStyle= "blue";
-        contexto.fillRect(this.posicion.x, this.posicion.y,
-                        this.width,this.height);
+        // contexto.fillStyle= "blue";
+        // contexto.fillRect(this.posicion.x, this.posicion.y,
+        //                 this.width,this.height);
+        contexto.drawImage(this.imagen,this.posicion.x,this.posicion.y);
 
     }
 }
@@ -54,8 +56,30 @@ class Jugador{
         this.posicion.y += this.velocidad.y;
 
     }
+}
+
+class Puntos{
+
+    //Nuestra clase jugador será la encargada de dibujar y accionar a pacman
+
+    constructor({posicion}){ //Le pasamos como parametro dos objetos con propiedades
+
+        this.posicion = posicion;
+        this.radio= 3;
+
+    }
+    dibujarPunto(){ //Función para pintar el pacman
+
+        contexto.beginPath();
+        contexto.arc(this.posicion.x ,this.posicion.y ,this.radio, 0, Math.PI*2);
+        contexto.fillStyle= "white";
+        contexto.fill();
+        contexto.closePath();
+
+    }
 
 }
+const puntos=[];
 const limites=[]; //Creamos un array de limites
 const jugador = new Jugador({ //Creamos nuestro jugador con propiedades
     posicion: {
@@ -91,12 +115,30 @@ const keys ={
 }
 let lastKey= '';
 const escenario=[
-    ['-','-','-','-','-','-',],
-    ['-',' ',' ',' ',' ','-',],
-    ['-',' ','-','-',' ','-',],
-    ['-',' ',' ',' ',' ','-',],
-    ['-','-','-','-','-','-',],
+    ['1','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','2',],
+    ['|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',],
+    ['|',' ','cL','cR',' ','1','cR',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','cL','2',' ','cL','cR',' ','|',],
+    ['|',' ',' ',' ',' ','|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ',' ',' ',' ','|',],
+    ['|',' ','cL','cR',' ','cB',' ','cL','-','-','-','-','-','-','-','-','cR',' ','cB',' ','cL','cR',' ','|',],
+    ['|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',],
+    ['4','-','-','-','2',' ','cT',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','cT',' ','1','-','-','-','3',],
+    ['.','.','.','.','|',' ','|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ','|','.','.','.','.',],
+    ['.','.','.','.','|',' ','|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',' ','|','.','.','.','.',],
+    ['1','-','-','-','3',' ','|',' ','cL','-','-','-','-','-','-','cR',' ','|',' ','4','-','-','-','2',],
+    ['|',' ',' ',' ',' ',' ','cB',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','cB',' ',' ',' ',' ',' ','|',],
+    ['|',' ','cL','cR',' ',' ','',' ',' ','cL','-','-','-','-','cR',' ',' ',' ',' ',' ','cL','cR',' ','|',],
+    ['|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',],
+    ['|',' ','cL','-','-','cR',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','cL','-','-','cR',' ','|',],
+    ['|',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ',' ','|',],
+    ['4','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','-','3',],
 ] //Creamos una matriz para representar nuestro escenario
+function crearImagen(src){
+    
+    const imagen= new Image();
+    imagen.src= src
+    return imagen;
+
+}
 escenario.forEach((row, i) =>{ 
 
     //Creamos el foreach para el escenario obteniendo los simbolos y cambiandolos por el mapa
@@ -112,10 +154,133 @@ escenario.forEach((row, i) =>{
                         posicion:{
                             x: Limites.width*j, //Le damos el número 40 para que se le asigne ese ancho y largo
                             y: Limites.height*i // Y lo multiplicamos por las variables indice para dar la posicion
-                        }
+                        },
+                        imagen: crearImagen('/ExamenSegundaUnidad/images/pipeHorizontal.png')
                     })
                 );
-                break;
+            break;
+            case '|':
+                limites.push(
+                      new Limites({
+                        posicion:{
+                               x: Limites.width*j, //Le damos el número 40 para que se le asigne ese ancho y largo
+                               y: Limites.height*i // Y lo multiplicamos por las variables indice para dar la posicion
+                          },
+                          imagen: crearImagen('/ExamenSegundaUnidad/images/pipeVertical.png')
+                  })
+                    );
+            break;
+            case '1':
+                    limites.push(
+                           new Limites({
+                              posicion:{
+                                x: Limites.width*j, //Le damos el número 40 para que se le asigne ese ancho y largo
+                                   y: Limites.height*i // Y lo multiplicamos por las variables indice para dar la posicion
+                             },
+                               imagen: crearImagen('/ExamenSegundaUnidad/images/pipeCorner1.png')
+                       })
+                        );
+            break;
+            case '2':
+                 limites.push(
+                        new Limites({
+                            posicion:{
+                                      x: Limites.width*j, //Le damos el número 40 para que se le asigne ese ancho y largo
+                                      y: Limites.height*i // Y lo multiplicamos por las variables indice para dar la posicion
+                                   },
+                             imagen: crearImagen('/ExamenSegundaUnidad/images/pipeCorner2.png')
+                          })
+                         );
+            break;
+            case '3':
+                    limites.push(
+                         new Limites({
+                                 posicion:{
+                                       x: Limites.width*j, //Le damos el número 40 para que se le asigne ese ancho y largo
+                                       y: Limites.height*i // Y lo multiplicamos por las variables indice para dar la posicion
+                                  },
+                                    imagen: crearImagen('/ExamenSegundaUnidad/images/pipeCorner3.png')
+                                })
+                             );
+             break;
+             case '4':
+                    limites.push(
+                        new Limites({
+                            posicion:{
+                                x: Limites.width*j, //Le damos el número 40 para que se le asigne ese ancho y largo
+                                y: Limites.height*i // Y lo multiplicamos por las variables indice para dar la posicion
+                                     },
+                                     imagen: crearImagen('/ExamenSegundaUnidad/images/pipeCorner4.png')
+                                 })
+                            );
+            break;
+            case 'b':
+                    limites.push(
+                            new Limites({
+                                posicion:{
+                                     x: Limites.width*j, //Le damos el número 40 para que se le asigne ese ancho y largo
+                                     y: Limites.height*i // Y lo multiplicamos por las variables indice para dar la posicion
+                                    },
+                                     imagen: crearImagen('/ExamenSegundaUnidad/images/block.png')
+                                 })
+                             );
+            break;
+            case 'cR':
+                limites.push(
+                        new Limites({
+                            posicion:{
+                                 x: Limites.width*j, //Le damos el número 40 para que se le asigne ese ancho y largo
+                                 y: Limites.height*i // Y lo multiplicamos por las variables indice para dar la posicion
+                                },
+                                 imagen: crearImagen('/ExamenSegundaUnidad/images/capRight.png')
+                             })
+                         );
+            break;
+            case 'cL':
+                limites.push(
+                        new Limites({
+                            posicion:{
+                                 x: Limites.width*j, //Le damos el número 40 para que se le asigne ese ancho y largo
+                                 y: Limites.height*i // Y lo multiplicamos por las variables indice para dar la posicion
+                                },
+                                 imagen: crearImagen('/ExamenSegundaUnidad/images/capLeft.png')
+                             })
+                         );
+            break;
+            case 'cB':
+                limites.push(
+                        new Limites({
+                            posicion:{
+                                 x: Limites.width*j, //Le damos el número 40 para que se le asigne ese ancho y largo
+                                 y: Limites.height*i // Y lo multiplicamos por las variables indice para dar la posicion
+                                },
+                                 imagen: crearImagen('/ExamenSegundaUnidad/images/capBottom.png')
+                             })
+                         );
+            break;
+            case 'cT':
+                limites.push(
+                        new Limites({
+                            posicion:{
+                                 x: Limites.width*j, //Le damos el número 40 para que se le asigne ese ancho y largo
+                                 y: Limites.height*i // Y lo multiplicamos por las variables indice para dar la posicion
+                                },
+                                 imagen: crearImagen('/ExamenSegundaUnidad/images/capTop.png')
+                             })
+                         );
+            break;
+            case ' ':  
+                puntos.push(
+                    new Puntos({
+                        posicion:{
+                            x: Limites.width*j + Limites.width /2, //Le damos el número 40 para que se le asigne ese ancho y largo
+                            y: Limites.height*i + Limites.height /2 // Y lo multiplicamos por las variables indice para dar la posicion
+                            }
+                        })
+                    );
+            break;
+
+
 
         }
 
@@ -123,24 +288,157 @@ escenario.forEach((row, i) =>{
 
 });
 
+//Creamos una función para detectar la colisión entre jugador y limtes
+function choqueJugador({pacman,limite}){
+
+    return (pacman.posicion.y - pacman.radio + pacman.velocidad.y <= limite.posicion.y + limite.height && 
+        pacman.posicion.x + pacman.radio + pacman.velocidad.x >= limite.posicion.x &&
+        pacman.posicion.y + pacman.radio + pacman.velocidad.y >= limite.posicion.y && 
+        pacman.posicion.x - pacman.radio + pacman.velocidad.x <= limite.posicion.x + limite.width);
+
+}
+
 function animacion(){ //Creamos una funcion que realizará el movimiento
 
     requestAnimationFrame(animacion); //Usamos el requestAnimation para crear un loop infinito
     contexto.clearRect(0,0, canvas.width,canvas.height); //Usamos clearRect para limpiar la pantalla
+    //Crearemos un if para que se le añada la velocidad a cada tecla
+    if(keys.w.pressed && lastKey=== 'w'){
+        
+        //Creamos un for para que recorra toods los límites, permitiendo que el jugador pueda moverse más fluido
+        // tanto de arriba abajo como de abajo hacia arriba y no haya trabas
+        for(let i=0; i<limites.length; i++){
+            const limite= limites[i];
+            if(choqueJugador({
+
+                pacman: {
+
+                    ...jugador, //Los tres puntos permiten obtener las propiedades del objeto del que se hace referencias
+                    velocidad:{
+
+                        x:0,
+                        y:-5
+
+                    }
+
+                },
+                limite: limite
+
+            })){
+            jugador.velocidad.y=0;
+            break;
+            }else{
+
+                jugador.velocidad.y=-5;
+
+            }
+        }
+    }
+    else if(keys.a.pressed && lastKey=== 'a'){
+        for(let i=0; i<limites.length; i++){
+        const limite= limites[i];
+            if(choqueJugador({
+
+                pacman: {
+
+                    ...jugador, //Los tres puntos permiten obtener las propiedades del objeto del que se hace referencias
+                    velocidad:{
+
+                        x:-5,
+                        y:0
+
+                    }
+
+                },
+                limite: limite
+
+            })){
+            jugador.velocidad.x=0;
+            break;
+            }else{
+
+                jugador.velocidad.x=-5;
+
+            }
+        }
+
+    }else if(keys.s.pressed && lastKey=== 's'){
+        for(let i=0; i<limites.length; i++){
+        const limite= limites[i];
+            if(choqueJugador({
+
+                pacman: {
+
+                    ...jugador, //Los tres puntos permiten obtener las propiedades del objeto del que se hace referencias
+                    velocidad:{
+
+                        x:0,
+                        y:5
+
+                    }
+
+                },
+                limite: limite
+
+            })){
+            jugador.velocidad.y=0;
+            break;
+            }else{
+
+                jugador.velocidad.y=5;
+
+            }
+        }
+    }else if(keys.d.pressed && lastKey=== 'd'){
+        for(let i=0; i<limites.length; i++){
+        const limite= limites[i];
+            if(choqueJugador({
+
+                pacman: {
+
+                    ...jugador, //Los tres puntos permiten obtener las propiedades del objeto del que se hace referencias
+                    velocidad:{
+
+                        x:5,
+                        y:0
+
+                    }
+
+                },
+                limite: limite
+
+            })){
+            jugador.velocidad.x=0;
+            break;
+            }else{
+
+                jugador.velocidad.x=5;
+
+            }
+        }
+    }
+    puntos.forEach((punto,i)=>{
+
+        punto.dibujarPunto();
+        if(Math.hypot(punto.posicion.x - jugador.posicion.x, 
+            punto.posicion.y - jugador.posicion.y) < punto.radio + jugador.radio){
+
+                puntos.splice(i,1)
+
+            }
+
+    })
     limites.forEach((limite)=>{ //Creamos un foreach para dibujar el mapa
 
         limite.dibujarLimite();
 
-        if( jugador.posicion.y - jugador.radio + jugador.velocidad.y <= limite.posicion.y + limite.height && 
-            jugador.posicion.x + jugador.radio + jugador.velocidad.x >= limite.posicion.x &&
-            jugador.posicion.y + jugador.radio + jugador.velocidad.y >= limite.posicion.y && 
-            jugador.posicion.x - jugador.radio + jugador.velocidad.x <= limite.posicion.x + limite.width){
-
+        if(choqueJugador({
+            pacman: jugador,
+            limite: limite})){
 
                 jugador.velocidad.x=0;
                 jugador.velocidad.y=0;
                
-
         }
     
     });
@@ -151,24 +449,6 @@ function animacion(){ //Creamos una funcion que realizará el movimiento
     jugador.velocidad.x=0;
     jugador.velocidad.y=0;
 
-    //Crearemos un if para que se le añada la velocidad a cada tecla
-    if(keys.w.pressed && lastKey=== 'w'){
-
-        jugador.velocidad.y=-5;
-        
-    }else if(keys.a.pressed && lastKey=== 'a'){
-
-        jugador.velocidad.x=-5
-
-    }else if(keys.s.pressed && lastKey=== 's'){
-
-        jugador.velocidad.y=5;
-        
-    }else if(keys.d.pressed && lastKey=== 'd'){
-        
-        jugador.velocidad.x=5;
-
-    }
 }
 animacion(); //Mandamos llamar la animación
 
